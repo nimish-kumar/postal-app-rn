6c345168c1cdd65f525cb3eaf1329704852997b8
Nimish Kumar <nimishk642@gmail.com>
2023-01-09 13:40:10 +0530

    Added apollo client to the main App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { TailwindProvider } from 'tailwind-rn';
@@ -5,15 +6,22 @@ import { TailwindProvider } from 'tailwind-rn';
 import RootNavigator from './src/navigator/RootNavigator';
 import utilities from './tailwind.json';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/ironic-kiwi',
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
     // @ts-ignore
     <TailwindProvider utilities={utilities}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <ApolloProvider client={client}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <RootNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
