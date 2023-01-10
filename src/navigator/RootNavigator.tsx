b760ecb0e553892e9b44d27417faec96e1ad9cf1
Nimish Kumar <nimishk642@gmail.com>
2023-01-11 04:43:07 +0530

    Added Modal screen to the root stack

--- src/navigator/RootNavigator.tsx
+++ src/navigator/RootNavigator.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
+import ModalScreen from '../screens/ModalScreen';
 import TabNavigator from './TabNavigator';
 
 export type RootStackParamList = {
@@ -8,13 +9,20 @@ export type RootStackParamList = {
   MyModal: { userId: string; name: string };
 };
 
-const RootStack = createNativeStackNavigator();
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
   return (
     <RootStack.Navigator>
       <RootStack.Group>
         <RootStack.Screen name="Main" component={TabNavigator} />
+      </RootStack.Group>
+      <RootStack.Group>
+        <RootStack.Screen
+          name="MyModal"
+          component={ModalScreen}
+          options={{ headerShown: false }}
+        />
       </RootStack.Group>
     </RootStack.Navigator>
   );
