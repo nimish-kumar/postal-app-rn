cc18795754248fd523f6a82e542f09eb51b17768
Nimish Kumar <nimishk642@gmail.com>
2023-01-11 04:43:43 +0530

    Added scrollable customer cards

--- src/screens/CustomerScreen.tsx
+++ src/screens/CustomerScreen.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@apollo/client';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import {
   CompositeNavigationProp,
@@ -6,9 +7,11 @@ import {
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { Image, Input } from '@rneui/themed';
 import React, { useLayoutEffect, useState } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 
+import CustomerCard from '../components/CustomerCard';
+import { GET_CUSTOMERS } from '../graphql/queries';
 import { RootStackParamList } from '../navigator/RootNavigator';
 import { TabNavigatorParamsList } from '../navigator/TabNavigator';
 
@@ -19,16 +22,16 @@ export type CustomerScreenNavigationProp = CompositeNavigationProp<
 
 const CustomerScreen = () => {
   const tw = useTailwind();
-  const navigation = useNavigation();
+  const navigation = useNavigation<CustomerScreenNavigationProp>();
   const [searchText, setSearchText] = useState<string>('');
-
+  const { loading, error, data } = useQuery(GET_CUSTOMERS);
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
   return (
-    <ScrollView style={{ backgroundColor: '#59C1CC' }}>
+    <View style={{ backgroundColor: '#59C1CC', flex: 1 }}>
       <Image
         source={{ uri: 'https://links.papareact.com/3jc' }}
         containerStyle={tw('w-full h-64')}
@@ -39,7 +42,16 @@ const CustomerScreen = () => {
         onChangeText={setSearchText}
         containerStyle={tw('bg-white pt-2 pb-0 px-10')}
       />
-    </ScrollView>
+      <ScrollView>
+        {data?.getCustomers
+          ?.filter(({ value: { name } }: CustomerList) =>
+            name.includes(searchText)
+          )
+          .map(({ name: ID, value: { email, name } }: CustomerResponse) => (
+            <CustomerCard key={ID} email={email} name={name} userID={ID} />
+          ))}
+      </ScrollView>
+    </View>
   );
 };
 
